c999bf33a617879e5d2d8df9ebc1bc6c812bb8dd
David Waller <david@krawaller.se>
2017-11-21 20:05:09 +0100

    typescript 1 solution

--- solutions/5_4_typescript/data.ts
+++ solutions/5_4_typescript/data.ts
@@ -1,6 +1,4 @@
-import { Review } from "./interfaces";
-
-export const movieData: Review = {
+export const movieData = {
   title: "Inception",
   imgUrl: "https://static.rogerebert.com/uploads/movie/movie_poster/inception-2010/large_ziKvu3Th9l1wN2aIeVj5ElpBqFu.jpg",
   review: "A very meta movie about dreams within dreams. And then there was a whole bunch of shooting, not really sure how that fit into the picture.",
