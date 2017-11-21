fd7fa7bcaa51653cef7e5ba6c107e73c279a6cd6
David Waller <david@krawaller.se>
2017-11-21 18:48:47 +0100

    solution alternates

--- solutions/3_6_data/code.js
+++ solutions/3_6_data/code.js
@@ -11,8 +11,22 @@ function renderMovie(data){
   document.querySelector(".infosection ul").innerHTML = actorList;
 
   /*
+  We could also have created each <li> individually:
+
+  for(let i=0; i<data.actors.length; i++){
+    let node = document.createElement("li");
+    node.innerText = data.actors[i];
+    document.querySelector(".infosection ul").appendChild(node);
+  }
+
   
-  Here's a cooler way to do it! :)
+  We could also create a big HTML string using joins:
+  
+  document.querySelector(".infosection ul").innerHTML =
+    "<li>" + data.actors.join("</li><li>") + "</li>";
+
+
+  Or, we could be a ninja and solve it with reduce! :)
   
   document.querySelector(".infosection ul").innerHTML = data.actors.reduce(
     (str,a)=>str + "<li>" + a + "</li>"),
