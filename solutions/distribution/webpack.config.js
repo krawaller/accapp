e280cd9bd7ae8c01c591a7ce5e67eef32859f973
David Waller <david@krawaller.se>
2018-03-13 07:42:20 +0100

    update solutions

--- solutions/distribution/webpack.config.js
+++ solutions/distribution/webpack.config.js
@@ -1,6 +1,18 @@
+var path = require("path");
+const WebpackNotifierPlugin = require("webpack-notifier");
+
 module.exports = {
+  mode: 'development',
   entry: './code.js',
   output: {
-    filename: 'distribution/bundle.js'
-  }
+    path: path.resolve(__dirname, "distribution"),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "distribution"),
+    port: 9000,
+  },
+  plugins: [
+    new WebpackNotifierPlugin({alwaysNotify: true}),
+  ],
 };
